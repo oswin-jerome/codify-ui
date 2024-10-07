106fe28b05241d5b3de34851663b5b6135ec3d36
Akashh Krishh <akashhkrishh@gmail.com>
2024-10-08 00:06:36 +0530

    initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen">
       <h1 className="text-6xl font-bold">
-        Codify <span className="bg-gradient-to-tr from-blue-500 to-pink-500 text-transparent bg-clip-text">Coming Soon</span>
+        Codify <span className="bg-gradient-to-tr from-blue-500 to-pink-500 text-transparent bg-clip-text">Coming Soon ...</span>
       </h1>
     </div>
   );
